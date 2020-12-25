481bc4875c906ae75f47e7b61bf830abaf9302c7
PyLord Vader <>
2020-12-24 22:13:37 -0800

    V1.0

--- webbot.py
+++ webbot.py
@@ -8,8 +8,9 @@ import time
 # ایجاد یک درایور مجازی از مرورگر کروم
 myChrome=webdriver.Chrome()
 # باز کردن صفحه وب به مدت 3 ثانیه
-myChrome.get("file:///C:/Users/mphpamatrasu2000/Desktop/uchihaBot/test.html")
-#myChrome.find_element_by_css_selector("input[value=blue]").click()
-#input_var=myChrome.find_element_by_css_selector("input[value=blue]")
-#print(input_var.is_selected())
+myChrome.get("C:\\Users\\mphpamatrasu2000\\Desktop\\uchihaBot\\test.html")
+myChrome.find_element_by_css_selector("input[value=blue]").click()
+input_var=myChrome.find_element_by_css_selector("input[value=blue]")
+if input_var.is_selected()==True:
+    print("Yes")
 time.sleep(3)
