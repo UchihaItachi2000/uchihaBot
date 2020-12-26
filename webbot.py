e25c857b697477ae2ecc93c57625ab556df9dffd
PyLord Vader <>
2020-12-26 15:50:24 -0800

    V1.0

--- webbot.py
+++ webbot.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from fake_useragent import UserAgent
+
 # وارد کردن ماژول تایم
 import time
 # وارد کردن ماژول رندوم
@@ -13,19 +16,42 @@ import string
 # غیر فعال کردن دسترسی وب سایت به ادرس شما
 
 # تابع ایجاد نام کاربری و رمز عبور
-def ir_generator(size=10, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+def ir_generator(size=20, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(10,size))
 
 options = webdriver.ChromeOptions()
+ua = UserAgent()
+userAgent = ua.random
+print(userAgent)
 options.add_argument("--disable-notifications")
+options.add_argument('--user-agent="Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166"')
 prefs = {"profile.default_content_setting_values.geolocation" :2}
 options.add_experimental_option("prefs",prefs)
-# ایجاد یک درایور مجازی از مرورگر کروم
-myChrome=webdriver.Chrome(chrome_options=options)
-myChrome.get("https://www.zoomit.ir")
-myChrome.maximize_window()
-myChrome.find_element_by_xpath("//*[@id='mainMenu']/div[2]/a[1]/i").click()
-myChrome.find_element_by_xpath("//*[@id='quickLoginRegisterIFrameParent']/div/div/div[2]/ul/li[2]/a").click()
-username=myChrome.find_element_by_xpath("/html/body/form/div[2]")
-username.send_keys(ir_generator())
-time.sleep(3)
+
+while 1==1:
+    # ایجاد یک درایور مجازی از مرورگر کروم
+    myChrome=webdriver.Chrome(chrome_options=options)
+    myChrome.get("https://www.zoomit.ir")
+    #myChrome.set_page_load_timeout(3)
+    #myChrome.maximize_window()
+    #myChrome.find_element_by_xpath("//*[@id='mainMenu']/div[2]/a[1]/i").click()
+    #myChrome.find_element_by_xpath("//*[@id='quickLoginRegisterIFrameParent']/div/div/div[2]/ul/li[2]/a").click()
+    #myChrome.switch_to.frame(myChrome.find_element_by_id("quickRegisterIframe"))
+    #myChrome.find_element_by_xpath("//*[@id='quickLoginRegisterIFrameParent']/div/div/div[2]/div[2]").click()
+    #myChrome.find_element_by_xpath("//*[@id='Username']").click()
+    #myChrome.implicitly_wait()
+    #username=myChrome.find_element_by_id("Username")
+    #username.send_keys(ir_generator())
+    #email=myChrome.find_element_by_id("EmailOrPhoneNumber")
+    #email.send_keys(ir_generator()+"@gmail.com")
+    #password=myChrome.find_element_by_id("Password")
+    #passs=int(''.join(random.choice(string.digits) for _ in range(10,20)))
+    #print(passs)
+    #password.send_keys(passs)
+    #print(passs)
+    #cpassword=myChrome.find_element_by_id("ConfirmPassword")
+    #print(passs)
+    #cpassword.send_keys(passs)
+    #myChrome.find_element_by_id("register").click()
+    #time.sleep(3)
+    myChrome.close()
