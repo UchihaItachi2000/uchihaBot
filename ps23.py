2883e1f09f526c418e31577d270bbebccc724ba0
PyLord Vader <>
2021-01-17 03:22:14 +0100

    V1.0

--- ps23.py
+++ ps23.py
@@ -2,6 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import time
 
 # مشخص کردن ویژگی های مورد نیاز
 op = webdriver.ChromeOptions()
@@ -11,7 +12,16 @@ op.add_experimental_option("prefs",prefs)
 
 # ایجاد صفحه مورد نظر توسط بات
 chrome=webdriver.Chrome(options=op)
-chrome.get('https://nl.pinterest.com/')
-chrome.save_screenshot('C:\\Users\\mphpamatrasu2000\\Desktop\\pic1.png')
-chrome.get_screenshot_as_file('C:\\Users\\mphpamatrasu2000\\Desktop\\pic2.jpg')
+chrome.get('https://www.youtube.com/watch?v=qnfYo7RFER0')
+chrome.find_element(By.XPATH,'//*[@id="dismiss-button"]').click()
+chrome.find_element(By.XPATH,'//*[@id="movie_player"]/div[28]/div[2]/div[2]/button[8]').click()
+
+t=0
+while t<300:
+    #chrome.save_screenshot('C:\\Users\\mphpamatrasu2000\\Desktop\\pics\\pic'+str(t)+'.png')
+    chrome.get_screenshot_as_file('C:\\Users\\mphpamatrasu2000\\Desktop\\pics\\pic'+str(t)+'.png')
+    chrome.minimize_window()
+    t=t+1
+    time.sleep(1)
+
 chrome.close()
