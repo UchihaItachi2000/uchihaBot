e56b4f1e01ac1f1cea962f55c1591bc96e36b9fe
PyLord Vader <>
2021-01-17 14:25:12 +0100

    V1.0

---
+++ view.py
@@ -0,0 +1,162 @@
+# مشخص کردن ماژول های مورد نیاز
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import threading
+import time
+
+
+
+def cht0():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+def cht1():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        #chrome.close()
+def cht2():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+def cht3():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+def cht4():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+def cht5():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+def cht6():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+def cht7():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+def cht8():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+def cht9():
+    while 1==1:
+        # مشخص کردن ویژگی های مورد نیاز
+        op = webdriver.ChromeOptions()
+        op.add_argument("--disable-notifications")
+        prefs = {"profile.default_content_setting_values.geolocation" :2}
+        op.add_experimental_option("prefs",prefs)
+        # ایجاد صفحه مورد نظر توسط بات
+        chrome=webdriver.Chrome(options=op)
+        chrome.get('...')
+        time.sleep(10)
+        print('OK')
+        chrome.close()
+
+x0 = threading.Thread(target=cht0)
+x1 = threading.Thread(target=cht1)
+x2 = threading.Thread(target=cht2)
+x3 = threading.Thread(target=cht3)
+x4 = threading.Thread(target=cht4)
+x5 = threading.Thread(target=cht5)
+x6 = threading.Thread(target=cht6)
+x7 = threading.Thread(target=cht7)
+x8 = threading.Thread(target=cht8)
+x9 = threading.Thread(target=cht9)
+
+
+x0.start()
+x1.start()
+x2.start()
+x3.start()
+x4.start()
+x5.start()
+x6.start()
+x7.start()
+x8.start()
+x9.start()
